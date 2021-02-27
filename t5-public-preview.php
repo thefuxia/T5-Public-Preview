c918e8cc9e3b8e0d1bacc1894497a4c2d41be93e
Fuxia  Scholz <hallo@fuxia.me>
2021-02-27 18:32:57 +0100

    Update t5-public-preview.php

--- t5-public-preview.php
+++ t5-public-preview.php
@@ -4,8 +4,8 @@
  * Description: Make drafts and pending posts publicly visible.
  * Plugin URI:  http://wordpress.stackexchange.com/q/107062/73
  * Version:     2013.10.03
- * Author:      Thomas Scholz
- * Author URI:  http://toscho.de
+ * Author:      Fuxia Scholz
+ * Author URI:  https://fuxia.me
  * Licence:     MIT
  * License URI: http://opensource.org/licenses/MIT
  * Textdomain:  plugin_t5_public_preview
